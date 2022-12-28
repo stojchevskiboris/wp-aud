680bbe5c627360a0f30e6c83a24b4524a810b5d3
Boris Stojchevski <stojcevskib.02@gmail.com>
2022-12-28 19:34:48 +0100

    Session management

--- src/main/java/mk/ukim/finki/wpaud/bootstrap/DataHolder.java
+++ src/main/java/mk/ukim/finki/wpaud/bootstrap/DataHolder.java
@@ -2,6 +2,7 @@ package mk.ukim.finki.wpaud.bootstrap;
 
 import javax.annotation.PostConstruct;
 import mk.ukim.finki.wpaud.model.Category;
+import mk.ukim.finki.wpaud.model.User;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -10,11 +11,14 @@ import java.util.List;
 @Component
 public class DataHolder {
     public static List<Category> categories = new ArrayList<>();
-
+    public static List<User> users = new ArrayList<>();
     @PostConstruct
     public void init(){
-        this.categories.add(new Category("Software", "Software Category"));
-        this.categories.add(new Category("Books", "Books Category"));
+        categories.add(new Category("Software", "Software Category"));
+        categories.add(new Category("Books", "Books Category"));
+        users.add(new User("boris.stojchevski","bs","Boris","Stojchevski"));
+        users.add(new User("admin","admin","admin","admin"));
+        users.add(new User("martin.martinov","mm","Martin","Martinov"));
     }
 
 }
