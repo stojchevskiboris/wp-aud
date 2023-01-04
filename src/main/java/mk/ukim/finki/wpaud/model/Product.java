4941090372f533f0f40047f64a0af664ecce10e9
Boris Stojchevski <stojcevskib.02@gmail.com>
2023-01-04 15:04:43 +0100

    JPA Mapping

--- src/main/java/mk/ukim/finki/wpaud/model/Product.java
+++ src/main/java/mk/ukim/finki/wpaud/model/Product.java
@@ -2,13 +2,22 @@ package mk.ukim.finki.wpaud.model;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
 @Data
+@Entity
 public class Product {
+    @Id
     private Long id;
     private String name;
     private Double price;
     private Integer quantity;
+    @ManyToOne
     private Category category;
+    @ManyToOne
     private Manufacturer manufacturer;
 
     public Product(String name, Double price, Integer quantity, Category category, Manufacturer manufacturer) {
@@ -18,5 +27,9 @@ public class Product {
         this.quantity = quantity;
         this.category = category;
         this.manufacturer = manufacturer;
+    }
+
+    public Product() {
+
     }
 }
--- src/main/java/mk/ukim/finki/wpaud/model/User.java
+++ src/main/java/mk/ukim/finki/wpaud/model/User.java
@@ -2,17 +2,29 @@ package mk.ukim.finki.wpaud.model;
 
 import lombok.Data;
 
+import javax.persistence.*;
+import java.util.List;
+
 @Data
+@Entity
+@Table(name = "shop_users")
 public class User {
+    @Id
     public String username;
     private String password;
     private String name;
     private String surname;
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
+    private List<ShoppingCart> carts;
 
     public User(String username, String password, String name, String surname) {
         this.username = username;
         this.password = password;
         this.name = name;
         this.surname = surname;
+    }
+
+    public User() {
+
     }
 }
